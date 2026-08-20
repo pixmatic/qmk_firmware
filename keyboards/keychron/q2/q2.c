9bbfd37bc1f9fd2186d7f3a21f61291b56bb9fa7
pixmatic <me@pixmatic.dev>
2026-08-20 17:19:56 +0200

    quitado switch win-mac y sus teclas

--- keyboards/keychron/q2/q2.c
+++ keyboards/keychron/q2/q2.c
@@ -20,9 +20,7 @@
 
 bool dip_switch_update_kb(uint8_t index, bool active) {
     if (!dip_switch_update_user(index, active)) { return false;}
-    if (index == 0) {
-        default_layer_set(1UL << (active ? 1 : 0));
-    }
+    // Switch reservado para uso futuro
     return true;
 }
 
